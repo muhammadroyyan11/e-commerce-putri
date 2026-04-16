7a017421f2234f48e4bfc0eb39c678989e8fc6b9
muhammadroyyan11 <royyanmz87@gmail.com>
2026-04-16 19:36:30 +0700

    logs

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -9,7 +9,9 @@ class PaymentMethodSeeder extends Seeder
 {
     public function run(): void
     {
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         PaymentMethod::truncate();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         PaymentMethod::insert([
             [
--- database/seeders/ShippingZoneSeeder.php
+++ database/seeders/ShippingZoneSeeder.php
@@ -9,7 +9,9 @@ class ShippingZoneSeeder extends Seeder
 {
     public function run(): void
     {
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0');
         ShippingZone::truncate();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         $zones = [
             [
